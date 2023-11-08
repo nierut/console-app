57ed2960bd3720fa9c2b8b6470c015c5113db58b
Rút Ní Eithir <rut.nieithir@hotmail.com>
2023-11-08 13:08:20 +0100

    creates method for processing numbers

--- ConsoleApp/src/Main.java
+++ ConsoleApp/src/Main.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         String parameterOne = "";
         String parameterTwo = "";
-        System.out.println("Hello world");
         if (args.length == 0) {
             System.out.println("You have given no parameters");
         } else {
@@ -21,16 +22,21 @@ public class Main {
 
         if (parameterOne.endsWith(".txt")) {
             System.out.println("We are going to read a file");
+            int[] numbers = getNumbersFromFile(parameterOne);
         } else {
             if (isValidNumber(parameterOne)) {
                 System.out.println("get numbers from standard input");
+                int[] numbers = getNumbersFromStdInput();
             } else {
                 System.out.println("First parameter is not valid, must be positive integer or path to text file");
+                int[] numbers = new int[1];
             }
         }
 
 
+
     }
+
 
     public static boolean isValidNumber(String parameter) {
         try {
@@ -40,5 +46,35 @@ public class Main {
             System.out.println(e.getMessage());
             return false;
         }
+    }
+
+    public static int[] getNumbersFromFile(String filePath) {
+        return null;
+    }
+
+    public static int[] getNumbersFromStdInput() {
+        return null;
+    }
+
+    public static List<Integer> processNumbers(int[] numbers) {
+        List<Integer> result = new ArrayList<>();
+        if(arrayIsEven(numbers)) {
+            for(int i = 0; i < numbers.length;i++) {
+                if(numbers[i] % 2 == 0) {
+                    result.add(numbers[i]);
+                }
+            }
+        } else {
+            for(int i = 0; i < numbers.length;i++) {
+                if(numbers[i] % 2 != 0) {
+                    result.add(numbers[i]);
+                }
+            }
+        }
+        return result;
+    }
+
+    public static boolean arrayIsEven(int[] numbers) {
+        return numbers.length % 2 == 0;
     }
 }
